cc9c7a85b78534196d6e59bcb127db98cff0c707
husniddin <atoullayevhusniddin@gmail.com>
2024-11-21 15:54:22 +0500

    vue lesson-6

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <h1>Life cycle methods!</h1>
+    <h1>Counter:{{ count }}</h1>
+    <h1>{{ num }}</h1>
+    <button @click="count++">Increment</button>
+    <button @click="IncrementNumber">IncrementNumber</button>
+    <button @click="toggleComponent">Toggle Component</button>
+
+  </div>
+  <HelloWorld v-if="toggle"/>
+</template>
+
+<script setup>
+import {onMounted, onBeforeMount, ref, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted} from 'vue';
+import HelloWorld from "./components/HelloWorld.vue";
+
+let num=1;
+const count=ref(1);
+const toggle=ref(true)
+const IncrementNumber=()=>{
+  num++;
+}
+
+const toggleComponent =()=>{
+  toggle.value=!toggle.value;
+}
+
+onMounted(()=>{
+  console.log("onMounted");
+});
+
+onBeforeMount(() => {
+  console.log('onBeforeMount');
+  
+});
+
+onBeforeUpdate(() => {
+  console.log('onBeforeUpdate');
+  
+});
+
+onUpdated(() => {
+  console.log('onUpdate');
+  
+});
+
+onBeforeUnmount(()=>{
+  console.log("onBeforeUnMounted");
+});
+
+onUnmounted(()=>{
+  console.log("onUnMounted");
+});
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="card">
+      <h1>Hello World</h1>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+import {onBeforeUnmount, onUnmounted } from 'vue'
+
+onBeforeUnmount(()=>{
+  console.log("onBeforeUnMounted h");
+});
+
+onUnmounted(()=>{
+  console.log("onUnMounted h");
+});
+
+
+</script>
+
+<style scoped>
+.card{
+  width: 400px;
+  height: 400px;
+  display: grid;
+  place-content: center;
+  margin: 50px;
+  background-color: #fee6e6;
+}
+</style>
